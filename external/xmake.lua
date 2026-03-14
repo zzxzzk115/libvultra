94dce59869aba79be699d22c0c8dcbe8085fe038
Kexuan Zhang <sc23kz@leeds.ac.uk>
2026-03-14 14:13:53 +0000

    chore: use vasset bundled spz and miniply, support VGaussianSplat for now.

--- external/xmake.lua
+++ external/xmake.lua
@@ -34,19 +34,6 @@ target("debug_draw")
     add_rules("utils.install.cmake_importfiles")
     add_rules("utils.install.pkgconfig_importfiles")
 
-target("miniply")
-    set_kind("static")
-    add_headerfiles("miniply/**.h")
-    add_includedirs("miniply", {public = true}) -- public: let other targets to auto include
-    add_files("miniply/**.cpp")
-
-target("spz")
-    set_kind("static")
-    add_headerfiles("spz/**.h")
-    add_includedirs("spz/src/cc", {public = true}) -- public: let other targets to auto include
-    add_files("spz/**.cc")
-    add_packages("zlib")
-
 target("vrdx")
     set_kind("static")
     add_headerfiles("vrdx/**.h")
