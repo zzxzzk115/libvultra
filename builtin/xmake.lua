5b536ebfe4a695dc63a5746889bc7cfc12b28827
Lazy_V <954294627@qq.com>
2026-04-09 20:26:10 +0100

    fix: use vshadersystem 0.8.1

--- builtin/xmake.lua
+++ builtin/xmake.lua
@@ -647,7 +647,7 @@ task_end()
 if is_plat("android") then
     add_requires("vshadersystem v0.6.2", { configs = { debug = is_mode("debug") }})
 else
-    add_requires("vshadersystem v0.8.0", { configs = { debug = is_mode("debug") }})
+    add_requires("vshadersystem v0.8.1", { configs = { debug = is_mode("debug") }})
 end
 
 target("vultra_builtin_assets")
